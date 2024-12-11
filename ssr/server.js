005bb6a9fbb678a05fe56a7750a0140e90eda9b3
Susan Kafle <kafle.susan671@outlook.com>
2024-12-11 15:59:32 +0545

    asdasdasd

--- ssr/server.js
+++ ssr/server.js
@@ -1,58 +1,36 @@
-require('@babel/register')({
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-});
-
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
-const axios = require('axios');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-const App = require('../src/App').default;
-const { Helmet } = require('react-helmet');
-const { StaticRouter } = require('react-router-dom');
-const htmlManipulator = require('../htmlManipulator');
-
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
 const app = express();
-const PORT = 3000;
-const buildPath = path.join(__dirname, '../build');
 
-app.use(express.static(buildPath));
-
-// Route for dynamic meta tags
-app.use('/', require('./route'));
-
-app.get('/*', (req, res) => {
-  const context = {};
-  const appMarkup = ReactDOMServer.renderToString(
-    React.createElement(
-      StaticRouter,
-      { location: req.url, context },
-      React.createElement(App)
-    )
-  );
-
-  const helmet = Helmet.renderStatic();
-
-  fs.readFile(path.join(buildPath, 'index.html'), 'utf8', (err, data) => {
+app.get("/",(req,res)=>{
+  const filePath = path.resolve(__dirname, '../build', 'index.html');
+ fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      console.error('Something went wrong:', err);
-      return res.status(500).send('Internal Server Error');
+      return console.log(err);
     }
-
-    if (context.url) {
-      return res.redirect(301, context.url);
-    }
-
-    return res.send(
-      data.replace(
-        '<div id="root"></div>',
-        `<div id="root">${appMarkup}</div>${helmet.title.toString()}${helmet.meta.toString()}`
-      )
-    );
+    data = data.replace(/__TITLE__/g, 'Home Page')
+    .replace(/__DESCRIPTION__/g, 'This is the Home Page')
+    res.send(data);
   });
-});
+})
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.get("/blog/:slug",(req,res)=>{
+  const filePath = path.resolve(__dirname, '../build', 'index.html');
+ fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    data = data.replace(/__TITLE__/g, 'Blog Page')
+    .replace(/__DESCRIPTION__/g, 'This is the Blog Page')
+    res.send(data);
+  });
+}
+)
+
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+} ) 
